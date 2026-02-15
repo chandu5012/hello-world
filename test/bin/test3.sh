85b4e71a44592b23d457bc4e32f772a104aa61f6
chandu5012 <85389422+chandu5012@users.noreply.github.com>
2026-02-15 17:20:21 +0530

    Update test3.sh

--- test/bin/test3.sh
+++ test/bin/test3.sh
@@ -9,3 +9,29 @@ if [[ -n "$TIME_ZONE" && "$TIME_ZONE" != "N/A" ]]; then
 else
   echo "[INFO] Timezone not provided (N/A). Skipping timezone Spark configs."
 fi
+
+# cleanup input (Windows CRLF safe)
+time_zone="$(echo "$time_zone" | tr -d '\r' | xargs)"
+
+# always apply log4j
+driver_opts="-Dlog4j.configuration=file:$LOG4J_CONFIG"
+exec_opts="-Dlog4j.configuration=file:$LOG4J_CONFIG"
+
+# apply timezone ONLY if user provided it
+if [[ -n "$time_zone" && "$time_zone" != "N/A" ]]; then
+  driver_opts="-Duser.timezone=$time_zone $driver_opts"
+  exec_opts="-Duser.timezone=$time_zone $exec_opts"
+
+  SPARK_CONF+=(
+    --conf "spark.sql.session.timeZone=$time_zone"
+  )
+fi
+
+# always add extraJavaOptions (single occurrence)
+SPARK_CONF+=(
+  --conf "spark.driver.extraJavaOptions=$driver_opts"
+  --conf "spark.executor.extraJavaOptions=$exec_opts"
+)
+
+echo "Effective timezone: ${time_zone:-N/A}"
+
